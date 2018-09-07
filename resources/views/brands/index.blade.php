7f8a77d256df3c6c143efc5fe1050d887d5e51bf
vagrant <vagrant@homestead>
2018-09-07 15:17:42 +0000

    del brands list h2

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -8,32 +8,30 @@
 		&gt;
 		<a href="{{ route('brands') }}">{{ (Session::get('locale') == 'en') ?  'BRAND' : '厂牌' }}</a>
 	</div>
-	@foreach($brands as $val)
-		<h2 class="title">
-			@if(Session::get('locale') == 'en')
-				{{ $val['en_title'] }}
-			@else
-				{{ $val['title'] }}
-			@endif
-		</h2>
-		<ul class="brand-list">			
-			<li>
-				<dl class="brand-item">
-					<dt><img src="/uploads/{{ $val['image'] }}" alt=""></dt>
-					<dd>
-						<ul class="product-item-point">
-							@if(Session::get('locale') == 'en')
-								{!! $val['en_description'] !!}
-							@else
-								{!! $val['description'] !!}
-							@endif
-						</ul>
-						<a href="{{ route('brands.show', $val['id']) }}" class="more4">{{ (Session::get('locale') == 'en') ?  'LEARN MORE' : '查看更多' }}</a>
-					</dd>
-				</dl>
-			</li>
+	<ul class="brand-list">
+		@foreach($brands as $val)
+				@if(Session::get('locale') == 'en')
+					{{ $val['en_title'] }}
+				@else
+					{{ $val['title'] }}
+				@endif
+				<li>
+					<dl class="brand-item">
+						<dt><img src="/uploads/{{ $val['image'] }}" alt=""></dt>
+						<dd>
+							<ul class="product-item-point">
+								@if(Session::get('locale') == 'en')
+									{!! $val['en_description'] !!}
+								@else
+									{!! $val['description'] !!}
+								@endif
+							</ul>
+							<a href="{{ route('brands.show', $val['id']) }}" class="more4">{{ (Session::get('locale') == 'en') ?  'LEARN MORE' : '查看更多' }}</a>
+						</dd>
+					</dl>
+				</li>
+		@endforeach
 	</ul>
-	@endforeach
 	<div class="brand-page">
 		{{ $brands->links() }}
 	<!-- <a href="#">1</a>
